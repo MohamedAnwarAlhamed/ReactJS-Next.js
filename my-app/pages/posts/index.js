4a7ffba320d543359188039f3bd8ef95de0b8dae
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2023-05-21 11:48:41 +0300

    use getStaticProps

--- my-app/pages/posts/index.js
+++ my-app/pages/posts/index.js
@@ -1,10 +1,25 @@
 // post component
 
-export default function Posts() {
+export default function Posts(props) {
     
     return (
-        <>
-            dynamic 
-        </>
+        <div>
+            dynamic
+            {props.posts.map(post => (
+                <div key={post.id}>
+                    <h2>{post.title}</h2>
+                </div>
+            )) }
+        </div>
     )
+}
+
+export async function getStaticProps() { 
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
+    const data = await res.json()
+    return {
+        props: {
+            posts: data
+        }
+    }
 }
