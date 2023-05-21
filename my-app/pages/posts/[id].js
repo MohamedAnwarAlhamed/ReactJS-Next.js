6c4bfd02faa8f4b8f3b96a571f10ce3b7f5cb1ce
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2023-05-21 22:46:45 +0300

    use getStaticPaths

--- my-app/pages/posts/[id].js
+++ my-app/pages/posts/[id].js
@@ -11,7 +11,22 @@ export default function Details(props) {
   )
 }
 
-export async function getServerSideProps(context) {
+export async function getStaticPaths() { 
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
+  const data = await res.json()
+  const paths = data.map(d => {
+    return {
+      params: {id: `${d.id}`}
+    }
+  })
+  return {
+    paths: paths,
+    fallback: false
+  }
+}
+
+
+export async function getStaticProps(context) {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)  
   const data = await res.json()
   return {
